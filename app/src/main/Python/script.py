a02048370c2c7bcae2d8d09a2e93c30f7d42ba52
mohan <mohana75@yahoo.com>
2020-11-13 20:42:01 +0530

    1st commit

---
+++ app/src/main/Python/script.py
@@ -0,0 +1,6 @@
+
+def helloworld ():
+    return "Hello world from Python"
+
+def hello():
+    return "I am Neeli invoked through Python."
---
+++ app/src/main/java/com/sofia/texttospeech/MainActivity.java
@@ -0,0 +1,203 @@
+package com.sofia.texttospeech;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+    private EditText editText;
+    private ImageView micButton;
+    private  TextView txtview;
+    private TextToSpeech myTTS;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       /* System.out.println("yyyy" + Manifest.permission.RECORD_AUDIO + "666  " +PackageManager.PERMISSION_GRANTED);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+
+            System.out.println("kkk");
+            checkPermission();
+        }*/
+
+        myTTS = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    myTTS.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        editText = findViewById(R.id.text);
+        micButton = findViewById(R.id.button);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        getTimeFromAndroid();
+
+        txtview = findViewById(R.id.text_view);
+
+        // Python invocation
+        if(!Python.isStarted())
+            Python.start(new AndroidPlatform(this));
+
+        Python py = Python.getInstance();
+        final PyObject pyObj = py.getModule("script");
+        PyObject obj = pyObj.callAttr("helloworld");
+        txtview.setText(obj.toString());
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                editText.setText("");
+                editText.setHint("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                micButton.setImageResource(R.drawable.ic_mic_black_off);
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                Python py = Python.getInstance();
+                final PyObject pyObj = py.getModule("script");
+                PyObject obj = pyObj.callAttr("hello");
+                editText.setText(data.get(0) + "..." + obj.toString());
+                
+                myTTS.speak(data.get(0),TextToSpeech.QUEUE_FLUSH, null);
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    micButton.setImageResource(R.drawable.ic_mic_black_24dp);
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
+
+    private void checkPermission() {
+        System.out.println(Build.VERSION.SDK_INT + "***" + Build.VERSION_CODES.M);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            System.out.println("Check Permission");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void speakWords(String speech) {
+        //speak straight away
+        myTTS.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private void getTimeFromAndroid() {
+        Calendar c = Calendar.getInstance();
+        int hours = c.get(Calendar.HOUR_OF_DAY);
+
+        String greeting = null;
+        if(hours>=1 && hours<=12){
+            greeting = "Good Morning";
+        } else if(hours>=12 && hours<=16){
+            greeting = "Good Afternoon";
+        } else if(hours>=16 && hours<=21){
+            greeting = "Good Evening";
+        } else if(hours>=21 && hours<=24){
+            greeting = "Good Night";
+        }
+        Toast.makeText(this, greeting, Toast.LENGTH_SHORT).show();
+        speakWords(greeting);
+    }
+
+}
